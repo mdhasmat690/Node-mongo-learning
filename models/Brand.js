b2ffab79fe2087947d4a50f9945f7a874724a748
mdhasmat690 <mdhasmat690@gmail.com>
2023-08-10 10:48:25 +0600

    module 9 added

--- models/Brand.js
+++ models/Brand.js
@@ -7,7 +7,7 @@ const brandSchema = mongoose.Schema(
     name: {
       type: String,
       trim: true,
-      required: [true, "Please provide a brnad name"],
+      required: [true, "Please provide a brand name"],
       maxLength: 100,
       unique: true,
       lowercase: true,
