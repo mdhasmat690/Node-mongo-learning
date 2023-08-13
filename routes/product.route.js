5accdb92e1972b2e5262417019a6e6ef8e4a23f0
mdhasmat690 <mdhasmat690@gmail.com>
2023-08-13 16:11:54 +0600

    finish

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
 const multer = require("multer");
-
 const uploader = require("../middleware/uploader");
+const verifyToken = require("../middleware/verifyToken");
+const authorization = require("../middleware/authorization");
 
+// router.use(verifyToken);
 router.post(
   "/file-upload",
   uploader.array("image"),
@@ -17,7 +19,11 @@ router.route("/bulk-delete").delete(productController.bulkDeleteProduct);
 router
   .route("/")
   .get(productController.getProducts)
-  .post(productController.createProduct);
+  .post(
+    verifyToken,
+    authorization("admin", "store-manage"),
+    productController.createProduct
+  );
 
 router
   .route("/:id")
